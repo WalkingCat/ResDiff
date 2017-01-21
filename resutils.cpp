88c98a77a98eb273c27d68004b997ac49597b08a
WalkingCat <walking.cat@hotmail.com>
2017-01-21 15:21:36 +0800

    implement comparing message tables

--- resutils.cpp
+++ resutils.cpp
@@ -100,5 +100,22 @@ std::map<std::wstring, std::wstring> parse_strings(const std::wstring & name, co
 
 std::map<std::wstring, std::wstring> parse_message_table(const std::vector<unsigned char>* data)
 {
+	map<wstring, wstring> ret;
+	wchar_t buf[16] = L"0x";
+	if (data != nullptr) {
+		auto msg_data = (PMESSAGE_RESOURCE_DATA)data->data();
+		for (DWORD i = 0; i < msg_data->NumberOfBlocks; ++i) {
+			PMESSAGE_RESOURCE_BLOCK msg_block = msg_data->Blocks + i;
+			auto msg_entry = (PMESSAGE_RESOURCE_ENTRY)(data->data() + msg_block->OffsetToEntries);
+			for (auto id = msg_block->LowId; id <= msg_block->HighId; ++id, ++msg_entry) {
+				_ultow_s(id, buf + 2, _countof(buf) - 2, 16);
+				if (msg_entry->Flags == 1) {
+					ret[buf] = wstring((wchar_t*)msg_entry->Text, msg_entry->Length / sizeof(wchar_t));
+				} else { /*TODO*/ }
+			}
+		}
+	}
+	return ret;
+
 }
 
