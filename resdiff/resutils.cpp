d7656d52c19d034b2ed43210014b741b5808f39f
WalkingCat <walking.cat@hotmail.com>
2018-04-18 00:15:26 +0800

    fix some 64bit warnings

--- resdiff/resutils.cpp
+++ resdiff/resutils.cpp
@@ -12,9 +12,9 @@ resource_data load_resource(const wstring& file) {
 			auto& rsrc = *(resource_data*)lParam;
 			bool ignore = false;
 			if (IS_INTRESOURCE(lpszType)) {
-				switch ((int)lpszType) {
-				case (int)RT_VERSION:		ignore = true; break;
-				case (int)RT_MANIFEST:		ignore = true; break;
+				switch ((size_t)lpszType) {
+				case (size_t)RT_VERSION:		ignore = true; break;
+				case (size_t)RT_MANIFEST:		ignore = true; break;
 				}
 			} else {
 				if (wcscmp(lpszType, L"MUI") == 0) ignore = true;
@@ -25,24 +25,24 @@ resource_data load_resource(const wstring& file) {
 					wstring type_name, name;
 
 					if (IS_INTRESOURCE(lpszType)) {
-						switch ((int)lpszType) {
-						case (int)RT_CURSOR:		type_name = L"CURSOR"; break;
-						case (int)RT_BITMAP:		type_name = L"BITMAP"; break;
-						case (int)RT_ICON:			type_name = L"ICON"; break;
-						case (int)RT_MENU:			type_name = L"MENU"; break;
-						case (int)RT_DIALOG:		type_name = L"DIALOG"; break;
-						case (int)RT_STRING:		type_name = L"STRING"; break;
-						case (int)RT_FONTDIR:		type_name = L"FONTDIR"; break;
-						case (int)RT_FONT:			type_name = L"FONT"; break;
-						case (int)RT_ACCELERATOR:	type_name = L"ACCELERATOR"; break;
-						case (int)RT_RCDATA:		type_name = L"RCDATA"; break;
-						case (int)RT_MESSAGETABLE:	type_name = L"MESSAGETABLE"; break;
-						case (int)RT_GROUP_CURSOR:	type_name = L"GROUP_CURSOR"; break;
-						case (int)RT_GROUP_ICON:	type_name = L"GROUP_ICON"; break;
-						case (int)RT_HTML:			type_name = L"HTML"; break;
+						switch ((size_t)lpszType) {
+						case (size_t)RT_CURSOR:			type_name = L"CURSOR"; break;
+						case (size_t)RT_BITMAP:			type_name = L"BITMAP"; break;
+						case (size_t)RT_ICON:			type_name = L"ICON"; break;
+						case (size_t)RT_MENU:			type_name = L"MENU"; break;
+						case (size_t)RT_DIALOG:			type_name = L"DIALOG"; break;
+						case (size_t)RT_STRING:			type_name = L"STRING"; break;
+						case (size_t)RT_FONTDIR:		type_name = L"FONTDIR"; break;
+						case (size_t)RT_FONT:			type_name = L"FONT"; break;
+						case (size_t)RT_ACCELERATOR:	type_name = L"ACCELERATOR"; break;
+						case (size_t)RT_RCDATA:			type_name = L"RCDATA"; break;
+						case (size_t)RT_MESSAGETABLE:	type_name = L"MESSAGETABLE"; break;
+						case (size_t)RT_GROUP_CURSOR:	type_name = L"GROUP_CURSOR"; break;
+						case (size_t)RT_GROUP_ICON:		type_name = L"GROUP_ICON"; break;
+						case (size_t)RT_HTML:			type_name = L"HTML"; break;
 						default: {
 								wchar_t buf[16] = {};
-								_itow_s((int)lpszType, buf, 10);
+								_ui64tow_s((size_t)lpszType, buf, _countof(buf), 10);
 								type_name = buf;
 								break;
 							}
@@ -53,7 +53,7 @@ resource_data load_resource(const wstring& file) {
 
 					if (IS_INTRESOURCE(lpszName)) {
 						wchar_t buf[16] = {};
-						_itow_s((int)lpszName, buf, 10);
+						_ui64tow_s((size_t)lpszName, buf, _countof(buf), 10);
 						name = buf;
 					} else name = wstring(L"\"") + lpszName + L"\"";
 					auto hrsrc = FindResourceW(hModule, lpszName, lpszType);
